d91865d890725191d845ee34d590596e943db12e
saramendezz <saramendez.infor01@gmail.com>
2024-04-08 18:38:43 +0200

    obstacles

--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -7,6 +7,7 @@ public class GroundTile : MonoBehaviour
     void Start()
     {
         groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
+        SpawnObstacle();
     }
 
     private void OnTriggerExit(Collider other)
@@ -19,5 +20,17 @@ public class GroundTile : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public GameObject obstaclePrefab;
+
+    void SpawnObstacle ()
+    {
+        //Choose random point to spawn the obstacle
+        int obstacleSpawnIndex = Random.Range(2, 5);
+        Transform spawnPoint = transform.GetChild(obstacleSpawnIndex).transform;
+
+        //Spawn the obstacle at the position
+        Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
     }
 }
