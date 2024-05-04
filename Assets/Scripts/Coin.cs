32193fb74befd63d1ae02ccda192194d07e1b39d
saramendezz <saramendez.infor01@gmail.com>
2024-05-04 13:25:00 +0200

    Added coins

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    public float turnSpeed = 90f;
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check if we collide with the player
+        if (other.gameObject.name != "Player")
+        {
+            return;
+        }
+
+        //Add to the player's score
+
+        //Destroy the coin
+        Destroy(gameObject);
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 0, turnSpeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/GroundTile.cs
+++ Assets/Scripts/GroundTile.cs
@@ -1,3 +1,4 @@
+using System.Net.NetworkInformation;
 using UnityEngine;
 
 public class GroundTile : MonoBehaviour
@@ -8,6 +9,7 @@ public class GroundTile : MonoBehaviour
     {
         groundSpawner = GameObject.FindObjectOfType<GroundSpawner>();
         SpawnObstacle();
+        SpawnCoins();
     }
 
     private void OnTriggerExit(Collider other)
@@ -19,12 +21,12 @@ public class GroundTile : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public GameObject obstaclePrefab;
 
-    void SpawnObstacle ()
+    void SpawnObstacle()
     {
         //Choose random point to spawn the obstacle
         int obstacleSpawnIndex = Random.Range(1, 4);
@@ -32,5 +34,33 @@ public class GroundTile : MonoBehaviour
 
         //Spawn the obstacle at the position
         Instantiate(obstaclePrefab, spawnPoint.position, Quaternion.identity, transform);
+    }
+
+    public GameObject coinPrefab;
+
+    void SpawnCoins()
+    {
+        int coinsToSpawn = 5;
+        for (int i = 0; i < coinsToSpawn; i++)
+        {
+            GameObject temp = Instantiate(coinPrefab);
+            temp.transform.position = GetRandomPoint(GetComponent<Collider>());
+        }
+    }
+
+    Vector3 GetRandomPoint(Collider collider)
+    {
+        Vector3 point = new Vector3(
+            Random.Range(collider.bounds.min.x, collider.bounds.max.x),
+            Random.Range(collider.bounds.min.y, collider.bounds.max.y),
+            Random.Range(collider.bounds.min.z, collider.bounds.max.z)
+        );
+        if (point != collider.ClosestPoint(point))
+        {
+            point = GetRandomPoint(collider);
+        }
+
+        point.y = 1;
+        return point;
     }
 }
