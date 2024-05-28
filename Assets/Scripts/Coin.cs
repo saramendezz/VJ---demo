d3a7f1d62417cb131917e86de941bf2084a1703c
saramendezz <saramendez.infor01@gmail.com>
2024-05-28 23:01:00 +0200

    coins sound

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections;
 
 public class Coin : MonoBehaviour
 {
@@ -7,28 +8,31 @@ public class Coin : MonoBehaviour
     public AnimationCurve curve;
 
     private Vector3 finalPosition;
+    private AudioSource audioSource;
 
     MainMenu menu;
+
     private void OnTriggerEnter(Collider other)
     {
-        
-        //Check if we collide with the player
+        // Check if we collide with the player
         if (other.gameObject.name != "Player")
         {
             return;
         }
 
-        //Add to the player's score
+        // Add to the player's score
         menu.incrementScore();
 
-        //Destroy the coin
-        Destroy(gameObject);
+        // Start the coroutine to play the sound and destroy the coin after a delay
+        StartCoroutine(DestroyAfterSound());
     }
 
     void Start()
     {
         menu = GameObject.FindObjectOfType<MainMenu>();
-        // finalPosition = gameObject.transform.position;
+        // Add an AudioSource component if not already present
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.clip = coinSound;
     }
 
     public void setPosition(Vector3 pos)
@@ -44,5 +48,17 @@ public class Coin : MonoBehaviour
         transform.SetPositionAndRotation(upPos, transform.rotation);
         //Debug.Log(upPos);
         //Debug.Log(finalPosition);
+    }
+
+    private IEnumerator DestroyAfterSound()
+    {
+        // Play the coin sound
+        audioSource.Play();
+
+        // Wait for 1 second
+        yield return new WaitForSeconds(1f);
+
+        // Destroy the coin
+        Destroy(gameObject);
     }
 }
